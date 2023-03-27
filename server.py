58b55eea1a68ff9d83cf4f7735210d7e8d358465
nkchambers <nkchames@gmail.com>
2023-03-26 22:04:08 -0700

    adding hello_flask for 3-26-23

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template  # Import Flask to allow us to create our app, 
                                             #added render_template to connect server to html file
-                                            
+
 
 app = Flask(__name__)    # Create a new instance of the Flask class called "app"
 
@@ -15,6 +15,21 @@ def success():
 @app.route('/hello/<string:name>/<int:num>')
 def hello(name, num):
     return f"Hello {name * num}"
+
+
+@app.route('/lists')
+def render_lists():
+    # Soon enough, we'll get data from a database, but for now, we're hard coding data
+    student_info = [
+        {'name' : 'Michael', 'age' : 35},
+        {'name' : 'John', 'age' : 30 },
+        {'name' : 'Mark', 'age' : 25},
+        {'name' : 'KB', 'age' : 27}
+    ]
+    return render_template("lists.html", random_numbers = [3,1,5], students = student_info)
+
+
+
 
 if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
     app.run(debug=True) 
